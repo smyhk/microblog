f54c7a28d87c1530ce0196a38e37bb3745f1e0fe
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-07-15 11:26:21 -0700

    Cleanup and formatting

--- app/models.py
+++ app/models.py
@@ -7,8 +7,8 @@ from flask_login import UserMixin
 
 # User loader to persist across application pages
 @login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(UserMixin, db.Model):
