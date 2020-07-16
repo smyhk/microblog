16bd2d518864dae5ff3a303cea60933a29403678
Steve Kedzie <seal.team.smyhk@gmail.com>
2020-07-15 17:44:29 -0700

    Email admins on errors and write logs

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
+import os
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -13,7 +14,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-# Email error when in production
+# Emailer and logger for errors in production
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -29,6 +30,17 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    # Logger
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 
 from app import routes, models, errors
